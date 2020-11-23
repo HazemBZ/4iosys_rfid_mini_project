f0480fcf6408e661d8230be7b84c06d6c51acb7f
HazemBZ <hazembenbz@gmail.com>
2020-11-23 05:19:35 +0100

    refactored code to be dynamically configurable while conforming to a certain standard

--- esp8266_app/esp8266_logic/esp8266_logic.ino
+++ esp8266_app/esp8266_logic/esp8266_logic.ino
@@ -5,14 +5,21 @@ const char* ssid = "PLANET_007"; // Broker server Netwoek
 const char* password = "hamatari";  // Network pwd
 const char* mqttServer ="192.168.1.10";  // Broker ip (raspi) 
 const int mqttPort = 1883;  // default MQTT port 
-const char* mqttUser = "";  // no cred for now
+const char* mqttUser = "";  // no credentials for now
 const char* mqttPassword = "";  // no cred for now
+const char* ID = "123"; // suppose this is a uid 
+const char* STATE = "I AM OK!->state"; // STATE struct mockup
+
+// CHANNELS
+const char* ADV_CHANNEL = "advertisement";
+const char* STATE_CHANNEL = "state/" + ID;
+const char* COMMAND_CHANNEL = "command/" + ID;
 
 // client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup() {
+void setup() {  // START SETUP
   Serial.begin(9600);
   // wifi connection 
   WiFi.begin(ssid, password);
@@ -27,7 +34,7 @@ void setup() {
   client.setCallback(callback);
   
   // connect to mqtt server
-  while(!client.connected()) {
+  while(!client.connected()) { // Keep trying to connect
     Serial.println("Connecting to MQTT ...");
     if (client.connect("ESP8266Client", mqttUser, mqttPassword)) {
       Serial.println("connected");
@@ -37,12 +44,13 @@ void setup() {
       delay(2000);
     }
   }
-  
-  client.publish("test_channel", "hello raspi channel");
-  client.subscribe("esp/data");
+  const id
+  //client.publish(ADV_CHANNEL, "123"); // publish once your id (last ip adresse byte) "suppose this is a unique string"
+  client.subscribe(COMMAND_CHANNEL); // listen for commands in this channel
 
-}
+} // --- END SETUP
 
+  // onMessage callback ( here filter and handle messages ) 
   void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print("Message arrived at topic: ");
     Serial.println(topic);
@@ -56,8 +64,9 @@ void setup() {
     Serial.print("----------");
   }
 
-void loop() {
+void loop() { // keep publishing state at regular intervals
   client.loop();
-  client.publish("test_channel", "hello raspi channel");
+  client.publish(ADV_CHANNEL, ID); // for now keep sending ID
+  client.publish(STATE_CHANNEL, STATE);
   delay(2000);
 }
